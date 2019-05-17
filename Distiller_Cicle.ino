4a35f8b5f95b2383db13b04606acaa804c9c5c54
unknown <theusmoraes@gmail.com>
2019-05-16 21:33:03 -0300

    Prototipo com lcd e sensor de temperatura

--- Distiller_Cicle.ino
+++ Distiller_Cicle.ino
@@ -1,26 +1,49 @@
-const int soneloidePin = 6; // Definir qual é a porta no arduino da Valvula Soneloide
-const int boia1Pin = 2; // Definir qual é a Porta do sensor de nivel d'agua
-const int boia2Pin = 3; // Definir qual é a Porta do sensor de nivel d'agua
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
+OneWire pino(5);
+DallasTemperature barramento(&pino);
+DeviceAddress sensor;
+LiquidCrystal_I2C lcd(0x3F,2,1,0,4,5,6,7,3, POSITIVE);
+const int resfriamentoPin = 4;
+const int soneloidePin = 6; 
+const int boia1Pin = 2; 
+const int boia2Pin = 3; 
 
 void setup() {
-  
-  // Setando a entrada ou saido do pino
+  pinMode (resfriamentoPin OUTPUT);
   pinMode (boia1Pin, INPUT);
   pinMode (boia2Pin, INPUT);
   pinMode (soneloidePin, OUTPUT);  
+  barramento.begin();
+  barramento.getAddress(sensor, 0);  
+  lcd.begin (16,2);
 }
 
 void loop() {
-  //Aqui estou botando o valor que a boia esta gerando numa variavel, sendo HIGH, a boia cheia d'água e LOW, a boia vazia.
+  barramento.requestTemperatures(); 
+  float temperatura = barramento.getTempC(sensor);
   int boia2_estado = digitalRead(boia2Pin);
   int boia1_estado = digitalRead(boia1Pin);
-  //Nesta parte, primeiramente se a primeira boia estiver "LOW" significa que precisa encher o recipiente, ja se a boia 2 tiver "HIGH" precisa parar de encher
   if  (boia1_estado == LOW){ 
     digitalWrite(soneloidePin,HIGH); 
   }else{
-  if (boia2_estado == HIGH) {
-    digitalWrite(soneloidePin,LOW);
+    if (boia2_estado == HIGH) {
+      digitalWrite(soneloidePin,LOW);
+    }
   }
+  lcd.setCursor (2,0);
+  lcd.print("Temperatura");
+  lcd.setCursor (5,1);
+  lcd.print (temperatura);
+  lcd.setCursor (10,1);
+  lcd.print ("C");
+  delay (250);
+  if (temperatura >= 30){
+    digitalWrite(resfriamentoPin,HIGH);
+  }else{
+    digitalWrite(resfriamento,LOW);
   }
 }
