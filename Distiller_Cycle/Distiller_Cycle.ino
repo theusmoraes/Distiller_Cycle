2985344f164cb8e46fb9ab0defc1dd88708237fc
DS_01 <DS_01@sesipiata.local>
2019-07-22 15:48:36 -0300

    Começando prototipo realmente final

--- Distiller_Cycle/Distiller_Cycle.ino
+++ Distiller_Cycle/Distiller_Cycle.ino
@@ -8,30 +8,62 @@ DallasTemperature barramento(&pino);
 DeviceAddress sensor;
 LiquidCrystal_I2C lcd(0x3F,2,1,0,4,5,6,7,3, POSITIVE);
 const int soneloidePin = 6; 
-const int boia1Pin = 2; 
-const int boia2Pin = 3; 
+const int SuperiorEnvia = 2; 
+const int InferiorEvia = 3; 
+const int InferiorArmazena =  4;
+const int BombaArmazena = 7
+int tempoFinalEnvia = 300;
+int tempoFinalResfriaBomba = 400;
+int tempo finalResfriaSoneloide = 400;
+long tempoAtual;
+long tempoInicial;
+int cont=0;
 
 void setup() {
-  pinMode (boia1Pin, INPUT);
-  pinMode (boia2Pin, INPUT);
+  pinMode (SuperiorEnvia, INPUT);
+  pinMode (InferiorEvia, INPUT);
   pinMode (soneloidePin, OUTPUT);  
+  pinMode (soneloideRua, OUTPUT);  
+  pinMode (InferiorArmazena, INPUT);
   barramento.begin();
+  
   barramento.getAddress(sensor, 0);  
   lcd.begin (16,2);
 }
 
 void loop() {
+  
   barramento.requestTemperatures(); 
   float temperatura = barramento.getTempC(sensor);
-  int boia2_estado = digitalRead(boia2Pin);
-  int boia1_estado = digitalRead(boia1Pin);
-  if  (boia1_estado == HIGH){ 
-    digitalWrite(soneloidePin,LOW); 
-  }else{
-    if (boia2_estado == LOW) {
+  int InferiorEvia = digitalRead(InferiorEvia);
+  int SuperiorEnvia_estado = digitalRead(SuperiorEnvia);
+  int InferiorArmazena_estado = digitalRead(InferiorArmazena);
+  tempoAtual = millis()/1000; //Atualizando sempre o tempo atual
+    if (InferiorEvia_estado == LOW) {//Se a boia inferior do enviar estiver low ligar soneloide
       digitalWrite(soneloidePin,HIGH);
+      if (cont = 1){                // Se for a primeira vez que entrou nesse ciclo registra o tempo inicial que começou
+        long TempoInicial = millis()/1000;
+      }
+      
+      cont = 1;
+      
+      
+    }else{
+      if (tempoAtual - tempoInicial >= tempoFinalEnvia) {
+        digitalWrite(SoneloidePin,LOW);
+        cont = 0; 
+        digitalWrite(BombaArmazena,HIGH);
+      }
     }
-  }
+    if (BombaArmazena = HIGH && InferiorArmazena = LOW){
+      digitalWrite(BombaArmazena,LOW);
+      TempoInicial = millis()/1000;
+      digitalWrite(SoneloideRua,HIGH);
+    }
+    if (SoneloideRua = HIGH && tempoAtual - tempoInicial >= tempoFinalResfriaSoneloide){
+      digitalWrite(SoneloideRua,LOW);
+    }
+  
   lcd.setCursor (2,0);
   lcd.print("Temperatura");
   lcd.setCursor (5,1);
@@ -40,3 +72,4 @@ void loop() {
   lcd.print ("C");
   delay (250);
 }
+
