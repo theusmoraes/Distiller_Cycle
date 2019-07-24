59343b602b553775eb57859b4cb34cd5a8a9173e
DS_01 <DS_01@sesipiata.local>
2019-07-24 15:39:01 -0300

    FIM???

--- Distiller_Cycle/Distiller_Cycle.ino
+++ Distiller_Cycle/Distiller_Cycle.ino
@@ -8,13 +8,14 @@ DallasTemperature barramento(&pino);
 DeviceAddress sensor;
 LiquidCrystal_I2C lcd(0x3F,2,1,0,4,5,6,7,3, POSITIVE);
 const int soneloidePin = 6; 
+const int soneloideRua = 6; 
 const int SuperiorEnvia = 2; 
 const int InferiorEvia = 3; 
 const int InferiorArmazena =  4;
-const int BombaArmazena = 7
+const int BombaArmazena = 7;
 int tempoFinalEnvia = 300;
 int tempoFinalResfriaBomba = 400;
-int tempo finalResfriaSoneloide = 400;
+int tempoFinalResfriaSoneloide = 400;
 long tempoAtual;
 long tempoInicial;
 int cont=0;
@@ -35,13 +36,13 @@ void loop() {
   
   barramento.requestTemperatures(); 
   float temperatura = barramento.getTempC(sensor);
-  int InferiorEvia = digitalRead(InferiorEvia);
+  int InferiorEvia_estado = digitalRead(InferiorEvia);
   int SuperiorEnvia_estado = digitalRead(SuperiorEnvia);
   int InferiorArmazena_estado = digitalRead(InferiorArmazena);
   tempoAtual = millis()/1000; //Atualizando sempre o tempo atual
     if (InferiorEvia_estado == LOW) {//Se a boia inferior do enviar estiver low ligar soneloide
       digitalWrite(soneloidePin,HIGH);
-      if (cont = 1){                // Se for a primeira vez que entrou nesse ciclo registra o tempo inicial que começou
+      if (cont == 0){                // Se for a primeira vez que entrou nesse ciclo registra o tempo inicial que começou
         long TempoInicial = millis()/1000;
       }
       
@@ -49,20 +50,21 @@ void loop() {
       
       
     }else{
-      if (tempoAtual - tempoInicial >= tempoFinalEnvia) {
-        digitalWrite(SoneloidePin,LOW);
+      if (soneloidePin == HIGH && tempoAtual - tempoInicial >= tempoFinalEnvia) { //Se a senloide tiver ligada e o tempo final ser alcançado, desliga soneloide liga bomba para re encher o armazena
+        digitalWrite(soneloidePin,LOW);
         cont = 0; 
         digitalWrite(BombaArmazena,HIGH);
+        tempoInicial = millis()/1000;
       }
     }
-    if (BombaArmazena = HIGH && InferiorArmazena = LOW){
+    if (BombaArmazena == HIGH && InferiorArmazena == LOW){ //Se bomba armazena estiver ligada e a agua estiver abaixo da boia liga solenoide rua
       digitalWrite(BombaArmazena,LOW);
-      TempoInicial = millis()/1000;
-      digitalWrite(SoneloideRua,HIGH);
+      digitalWrite(soneloideRua,HIGH);
     }
-    if (SoneloideRua = HIGH && tempoAtual - tempoInicial >= tempoFinalResfriaSoneloide){
-      digitalWrite(SoneloideRua,LOW);
+    if (soneloideRua == HIGH && tempoAtual - tempoInicial >= tempoFinalResfriaSoneloide){ //A soneloide estando ligada ira desligar quando o tempo final ser alcançado
+      digitalWrite(soneloideRua,LOW);
     }
+    
   
   lcd.setCursor (2,0);
   lcd.print("Temperatura");
